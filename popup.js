3bdba121abeba8f9f0a9367556b3eab63e920175
c0d3rm0nk3y <code4chaosmobile@gmail.com>
2014-11-26 16:53:39 -0600

    base code load

---
+++ popup.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  document.getElementById('submit').onclick = function() {
+    console.log('sumbit clicked..');
+
+  }
+
+  document.getElementById('login').onclick = function() {
+    console.log('login clicked..');
+
+  }
+}
