ecea709e5ad500bd3bc578b4abb24f17b0209e33
c0d3rm0nk3y <code4chaosmobile@gmail.com>
2014-11-26 17:11:25 -0600

    correction, correct code

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'googleAuth' : {
+		'clientID' 		: '116823597102-72kant6pj0pog35cilnmh61oevon2ilp.apps.googleusercontent.com',
+		'clientSecret' 	: 'G-sMmV3fCzGUNEoVPLpGsyVk',
+		'callbackURL' 	: 'http://monkey-nodejs-71725.usw1.nitrousbox.com:8080/auth/google/callback'
+	}
+
+};
---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://datawhore:badCodeMonkey01!@ds027799.mongolab.com:27799/news' 
+};
