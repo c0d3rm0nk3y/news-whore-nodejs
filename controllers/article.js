ac021d951b59b64aa2755c40b27f97518a780504
c0d3rm0nk3y <code4chaosmobile@gmail.com>
2014-12-06 18:12:39 +0000

    changing passport startegy

---
+++ controllers/article.js
@@ -0,0 +1,69 @@
+var Article = require('../models/article');
+var newswhore = require('./newswhore');
+
+// Create endpoint /api/articles for POSTS
+exports.postArticles = function(req, res) {  
+  var link = req.body.link;
+  var uId = req.user._id;
+  // check if article exists
+  Article.findOne({ userId: uId, link: link }, function(err, foundArt) {
+    if(err) { // not found or otheris
+      res.send(err);
+      // run article through the wash..
+      
+    } else {  // found
+      if(foundArt !== null) {
+        res.json({message: 'article already submitted', data: foundArt});
+      } else {
+        newswhore.buildArticle(link, uId).then(
+          function(result) {
+            res.json(result);
+          }, function(err) {
+            res.send(err);
+          }
+        );
+      }
+    }
+  });
+};
+
+// Create endpoint /api/articles for GET
+exports.getArticles = function(req, res) {
+  console.log('postArticles().. \nquery: %s\nuser: %s', req.query, req.user);
+  console.log('postArticles().. filter: ', req.query.filter);
+  var filter = req.query.filter;
+  filter = filter.replace('+', ' ');
+  Article.find({userId: req.user._id}, filter , function(err, artsFnd) {
+    if(err) { res.send(err); }
+    else {
+      res.json(artsFnd);
+    }
+  });
+};
+
+// Create endpoint /api/articles/:article_id for GET
+exports.getArticle = function(req, res) { 
+  console.log('getArticle():article_id');
+  Article.find({ userId: req.user._id, _id: req.params.article_id }, function(err, artFnd) {
+    if(err) { res.send(err); } // not found or otherise  
+    else { res.json(artFnd); }  // found
+  });
+};
+
+// Create endpoint /api/articles/:article_id for PUT
+exports.putArticle = function(req, res) {
+  console.log('putArticle():article_id');
+  Article.update({ userId: req.user._id, _id: req.params.article_id }, { link: req.body.link }, function(err, link, raw) {
+    if(err) { res.send(err); }
+    else    { res.json({ message: link + updated}); }
+  });
+};
+
+// Create endpoint /api/articles/:article_id for DELETE
+exports.deleteArticle = function(req, res) {
+  console.log('deleteArticle():article_id');
+  Article.remove( { userId: req.user._id, _id: req.paramas.article_id }, function(err) {
+    if(err) { res.send(err); }
+    else    { res.json({ message: 'Article removed from locker'}); }
+  });
+};
